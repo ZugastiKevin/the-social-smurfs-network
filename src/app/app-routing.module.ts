166284cc5cad55f18d83954c0e140f8b45dedc6b
Zugasti Kevin <rochambeaut@gmail.com>
2023-01-25 19:48:36 +0100

    feat: add routing root to smurfs and add routes smurfs and import component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SmurfsComponent } from './smurfs/smurfs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'smurfs', component: SmurfsComponent },
+  { path: '', redirectTo: 'smurfs', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
