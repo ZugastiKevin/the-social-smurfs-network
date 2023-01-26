d1316504534832ca5395a6dc6aecf803d16b989d
Zugasti Kevin <rochambeaut@gmail.com>
2023-01-26 17:23:52 +0100

    feature: add routes on module smurfs

--- src/app/smurfs/smurfs.module.ts
+++ src/app/smurfs/smurfs.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListSmurfsComponent } from './list-smurfs/list-smurfs.component';
 import { DetailSmurfComponent } from './detail-smurf/detail-smurf.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const smurfsRoutes: Routes = [
+  { path: 'smurfs', component: ListSmurfsComponent },
+  { path: 'smurf/:id', component: DetailSmurfComponent },
+];
 
 @NgModule({
   declarations: [
     ListSmurfsComponent,
-    DetailSmurfComponent
+    DetailSmurfComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(smurfsRoutes),
   ]
 })
 export class SmurfsModule { }
