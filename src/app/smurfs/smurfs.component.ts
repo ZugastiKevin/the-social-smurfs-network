9e7dbbb6ccb70e49f3d397589f5e3d953d87276b
Zugasti Kevin <rochambeaut@gmail.com>
2023-01-26 14:34:06 +0100

    feat: add smurfs

---
+++ src/app/smurfs/smurfs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smurfs',
+  templateUrl: './smurfs.component.html',
+  styleUrls: ['./smurfs.component.sass']
+})
+export class SmurfsComponent {
+
+}
