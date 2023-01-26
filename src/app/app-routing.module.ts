07c3b34de594d519c43b4c19c760ab7414f33814
Zugasti Kevin <rochambeaut@gmail.com>
2023-01-26 16:56:15 +0100

    refactor: remove useles call smurfs component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SmurfsComponent } from './smurfs/smurfs.component';
 
 const routes: Routes = [
-  { path: 'smurfs', component: SmurfsComponent },
   { path: '', redirectTo: 'smurfs', pathMatch: 'full' },
 ];
 
